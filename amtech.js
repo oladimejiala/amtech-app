addf1342ad408cdacba885b02aee8188108ac8f5
Developer_Mike <oladimejialabim@gmail.com>
2021-06-25 03:28:38 +0100

    updated This is developed from npm create react app on vscode on 23rd June, now commited to amtech app repo

--- amtech.js
+++ amtech.js
@@ -1,14 +1,86 @@
-import react from "react";
-import react-dom from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ".App-logo";
 
 
-function App () {
-  return <h1>AMTECH App</h1>;
+
+ReactDOM.render (
+
+
+)
+
+function Amtech({ company, zone, city, department, Country, Region, number }) {
+  return (
+    <div id="" className="center">
+      <h1>Welcome to {company}!</h1>
+      <p>{zone}</p>
+      <p>Situated at: {city}</p>
+      <p>{Country}</p>
+      <p>{Region}</p>
+      <h4>Our  subscribers:{number} </h4>
+    </div>
+  )
 };
 
 ReactDOM.render(
-  <App/>
-)
-document.getElementById("root")
+  <Amtech
+    company="AMTECH"
+    Zone="Aidegnon"
+    city="Abomey-Calavi"
+    department="Atlantique"
+    Country="Benin Republic"
+    Region="West-Africa"
+    number={10}
+  />,
+
+  document.getElementById("root")
 );
+
+
+
+const Login = {
+  "h3",
+    "<input type="email" placeholder="enter your email here"/>",
+      "<input type="email" placeholder="enter your email here"/>",
+};
+
+ReactDOM.render(
+React.createElement (
+"div",{
+    Email:
+    <input type="email" placeholder="enter your email here" />
+    Password:
+    <input type="email" placeholder="enter your email here" />
+}, any), document.getElementById("root")
+);
+
+
+  
+
+
+
+// function Register({login, signup}) {
+//   return(
+    
+//   )
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();

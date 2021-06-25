112c3a0a6e9271b7afc6c0cddeb18247c27e6ebd
Developer_Mike <oladimejialabim@gmail.com>
2021-06-25 03:19:00 +0100

    codesandbox1 This is the first commit on codesandbox in the development of react app

---
+++ amtech.js
@@ -0,0 +1,14 @@
+import react from "react";
+import react-dom from "react-dom";
+import "./index.css";
+
+
+function App () {
+  return <h1>AMTECH App</h1>;
+};
+
+ReactDOM.render(
+  <App/>
+)
+document.getElementById("root")
+);
